d677d05a02061c055bece905176b9a37af3b49e9
halleluujahh <thutranzzzzzz@gmail.com>
2024-01-11 21:32:47 +0700

    login

---
+++ src/java/dao/CommonDao.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import context.DBContext;
+import entity.Account;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author admin
+ */
+public class CommonDao extends DBContext{
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    
+    public Account CheckExistOfAcc(Account account) {
+        try {
+            connection = this.getConnection();
+
+            String sql = "SELECT a.Email, a.Password "
+                    + "FROM Account a "
+                    + "WHERE a.Email = ? AND a.Password = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, account.getEmail());
+            preparedStatement.setString(2, account.getPassword());
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Account foundAccount = new Account();
+                foundAccount.setId(resultSet.getInt("id"));
+                foundAccount.setEmail(resultSet.getString("username"));
+                foundAccount.setPassword(resultSet.getString("password"));
+                return foundAccount;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    
+    
+}
