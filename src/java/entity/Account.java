a9ac959d966d68cd16e0318d144da3e6f2facb22
Ductnhe170334 <116861261+AnnoyedPumpkin@users.noreply.github.com>
2024-01-13 21:57:55 +0700

    Add admin dashboard front-end

---
+++ src/java/entity/Account.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ *
+ * @author admin
+ */
+@Data
+@Builder
+@lombok.AllArgsConstructor
+@lombok.NoArgsConstructor
+public class Account {
+    private int id;
+    private String email;
+    private String password;
+}
