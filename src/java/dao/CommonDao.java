3bc9a18946fe663cc5bed651de8f1b3bd4881936
AnnoyedPumpkin <snowhead2003@gmail.com>
2024-01-15 15:39:45 +0700

    Fix bug login, register

---
+++ src/java/dao/CommonDao.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import context.DBContext;
+import entity.Account;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author admin
+ */
+
+public class CommonDao extends DBContext {
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+
+    public Account CheckExistOfAcc(Account account) {
+        try {
+            connection = this.getConnection();
+
+            String sql = "SELECT a.Email, a.Password "
+                    + "FROM Account a "
+                    + "WHERE a.Email = ? AND a.Password = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, account.getEmail());
+            preparedStatement.setString(2, account.getPassword());
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Account foundAccount = new Account();
+                foundAccount.setEmail(resultSet.getString("email"));
+                foundAccount.setPassword(resultSet.getString("password"));
+                return foundAccount;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+   public boolean CheckAccount(Account account) {
+    try {
+        connection = this.getConnection();
+
+        String sql = "SELECT * FROM [Account] WHERE Email = ?";
+        preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, account.getEmail());
+
+        resultSet = preparedStatement.executeQuery();
+
+        return resultSet.next(); // Trả về true nếu tìm thấy tài khoản, ngược lại trả về false
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+    }
+}
+
+public boolean CreateAccount(Account account) {
+    try {
+        connection = this.getConnection();
+
+        String sql = "INSERT INTO [dbo].[Account] ([Email], [Password]) VALUES (?, ?)";
+        preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, account.getEmail());
+        preparedStatement.setString(2, account.getPassword());
+
+        int affectedRows = preparedStatement.executeUpdate();
+
+        return affectedRows > 0; // Trả về true nếu có bản ghi được chèn thành công, ngược lại trả về false
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+    }
+}
+
+}
---
+++ src/java/entity/Product_Detail.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import lombok.Builder;
+import lombok.Data;
+/**
+ *
+ * @author Win 10
+ */
+@Data
+@Builder
+@lombok.AllArgsConstructor
+@lombok.NoArgsConstructor
+public class Product_Detail {
+    private int id;
+    private int product_id;
+    private int color_id;
+    private int category_id;
+    private int size_id;
+    private int brand_id;
+    private int discount_id;
+    private double stock;
+    private String description;
+    private String image;  
+}
