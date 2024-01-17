d46a013ae51e40616c84dbbedd6ffa932c9d025e
halleluujahh <thutranzzzzzz@gmail.com>
2024-01-17 14:09:52 +0700

    update format email login

--- src/java/control/AuthentController.java
+++ src/java/control/AuthentController.java
@@ -9,27 +9,27 @@ import dao.CommonDao;
 import entity.Account;
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author LENOVO
  */
-
 @WebServlet(name = "AuthentController", urlPatterns = {"/authen"})
 public class AuthentController extends HttpServlet {
+
     CommonDao commonDAO;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String url ="";
+        String url = "";
         String action = request.getParameter("action") == null ? "login" : request.getParameter("action");
         switch (action) {
             case "login":
@@ -52,7 +52,7 @@ public class AuthentController extends HttpServlet {
         }
         request.getRequestDispatcher(url).forward(request, response);
     }
-  
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -71,34 +71,36 @@ public class AuthentController extends HttpServlet {
         String remember = request.getParameter("remember");
         String email = request.getParameter("username");
         String password = request.getParameter("password");
-        if(!email.matches(Constant.EMAIL_REGEX)){
+        if (!email.matches(Constant.EMAIL_REGEX)) {
             request.setAttribute("err", "Email khong hop le");
-        }
-        Account account = Account.builder()
-                .email(email)
-                .password(password)
-                .build();
-        account = commonDAO.CheckExistOfAcc(account);
-
-        if (account == null) {
-            request.setAttribute("err", "Nhap sai ten dang nhap hoac mat khau");
             request.getRequestDispatcher("login.jsp").forward(request, response);
         } else {
+            Account account = Account.builder()
+                    .email(email)
+                    .password(password)
+                    .build();
+            account = commonDAO.CheckExistOfAcc(account);
 
-            HttpSession session = request.getSession();
-            session.setAttribute(Constant.SESSION_ACCOUNT, account);
-            Cookie userC = new Cookie("userC", email);
-            Cookie passC = new Cookie("passC", password);
-            if (remember != null) {
-                userC.setMaxAge(60 * 60 * 24);
-                passC.setMaxAge(60 * 60 * 24);
+            if (account == null) {
+                request.setAttribute("err", "Nhap sai ten dang nhap hoac mat khau");
+                request.getRequestDispatcher("login.jsp").forward(request, response);
             } else {
-                userC.setMaxAge(0);
-                passC.setMaxAge(0);
+
+                HttpSession session = request.getSession();
+                session.setAttribute(Constant.SESSION_ACCOUNT, account);
+                Cookie userC = new Cookie("userC", email);
+                Cookie passC = new Cookie("passC", password);
+                if (remember != null) {
+                    userC.setMaxAge(60 * 60 * 24);
+                    passC.setMaxAge(60 * 60 * 24);
+                } else {
+                    userC.setMaxAge(0);
+                    passC.setMaxAge(0);
+                }
+                response.addCookie(userC);
+                response.addCookie(passC);
+                response.sendRedirect("home");
             }
-            response.addCookie(userC);
-            response.addCookie(passC);
-            response.sendRedirect("home");
         }
     }
 
